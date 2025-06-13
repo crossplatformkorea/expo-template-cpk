4c1beebc669d0bd8592d420bd301dd7b39b952ec
Hyo <hyo@hyo.dev>
2025-06-14 03:51:20 +0900

    fix: tests

--- test/testSetup.ts
+++ test/testSetup.ts
@@ -18,9 +18,31 @@ global.__reanimatedWorkletInit = jest.fn();
 
 jest.mock('expo-router');
 jest.mock('@expo/react-native-action-sheet');
+
 jest.mock('expo-font', () => {
   return {useFonts: () => []};
 });
+
+jest.mock('expo-localization', () => ({
+  locale: 'en',
+  locales: ['en'],
+  timezone: 'Asia/Seoul',
+  isoCurrencyCodes: ['USD'],
+  region: 'US',
+  isRTL: false,
+  getLocales: () => [
+    {
+      languageTag: 'en',
+      languageCode: 'en',
+      regionCode: 'US',
+      currencyCode: 'USD',
+      currencySymbol: '$',
+      decimalSeparator: '.',
+      digitGroupingSeparator: ',',
+    },
+  ],
+}));
+
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 
